eb2edd05909a656ccee3c4d6a2fdf834e8bedd21
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2016-12-07 17:35:52 +0100

    implement u2f backend (see https://gist.github.com/jaxbot/2d64a4c46d6b5a3dd67d by @jaxbot)

--- routes/u2f.js
+++ routes/u2f.js
@@ -1,29 +1,55 @@
 'use strict'
 
-// var u2f = require('u2f')
-
-// var APP_ID = "http://localhost:3000"
+var insecurity = require('../lib/insecurity')
+var u2f = require('u2f')
 
 exports.registerRequest = function () {
   return function (req, res) {
-    console.log('TODO: U2F register request')
+    var user = insecurity.authenticatedUsers.from(req)
+    var authRequest = u2f.request(req.headers.host)
+    user.authRequest = authRequest
+    res.send(JSON.stringify(authRequest))
   }
 }
 
 exports.register = function () {
   return function (req, res) {
-    console.log('TODO: U2F register')
+    var user = insecurity.authenticatedUsers.from(req)
+    var checkRes = u2f.checkRegistration(
+      user.authRequest,
+      req.body
+    )
+    if (checkRes.successful) {
+      user.publicKey = checkRes.publicKey
+      user.keyHandle = checkRes.keyHandle
+      res.send(true)
+    } else {
+      res.send(checkRes.errorMessage)
+    }
   }
 }
 
 exports.signRequest = function () {
   return function (req, res) {
-    console.log('TODO: U2F sign request')
+    var user = insecurity.authenticatedUsers.from(req)
+    var authRequest = u2f.request(req.headers.host, user.authRequest.keyHandle)
+    user.authRequest = authRequest
+    res.send(JSON.stringify(authRequest))
   }
 }
 
 exports.sign = function () {
   return function (req, res) {
-    console.log('TODO: U2F sign')
+    var user = insecurity.authenticatedUsers.from(req)
+    var checkRes = u2f.checkSignature(
+      user.authRequest,
+      req.body,
+      user.publicKey
+    )
+    if (checkRes.successful) {
+      res.send({ success: true })
+    } else {
+      res.send({ error: checkRes.errorMessage })
+    }
   }
 }

212cd8e548a0d2faf01863d307b0b23522f3908d
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2016-12-07 07:28:26 +0100

    Add U2F to backend (with empty routes)

---
+++ routes/u2f.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// var u2f = require('u2f')
+
+// var APP_ID = "http://localhost:3000"
+
+exports.registerRequest = function () {
+  return function (req, res) {
+    console.log('TODO: U2F register request')
+  }
+}
+
+exports.register = function () {
+  return function (req, res) {
+    console.log('TODO: U2F register')
+  }
+}
+
+exports.signRequest = function () {
+  return function (req, res) {
+    console.log('TODO: U2F sign request')
+  }
+}
+
+exports.sign = function () {
+  return function (req, res) {
+    console.log('TODO: U2F sign')
+  }
+}
